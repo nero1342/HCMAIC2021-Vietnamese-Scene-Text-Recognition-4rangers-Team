55b01e3ae01559bbcac2779b0845950b75206102
Nhat Hoang-Xuan <hxnhat@selab.hcmus.edu.vn>
2022-08-16 14:16:27 +0000

    [INIT] First commit

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # HCMAIC2021-4RANGERS
+First Prize in AI-Challenge 2021, Ho Chi Minh City
+
+# Team members:
+- Nguyễn E Rô
+- Nguyễn Trường Hải
+- Nguyễn Ngọc Anh Khoa
+- Hà Đức Minh Thảo
+
+# Method
+- Three-stage approach
+    - Detection: YoloV5
+    - Recognition: SATRN (MMOCR)
+    - Post-processing: 
+        <!-- -  
+        -  -->
+
+# Installation
+## Requirements
+```
+    pip install -r requirements.txt
+    cd source/det/yolov5/
+    pip install -qr requirements.txt
+    pip install mmcv-full==1.4.1 -f https://download.openmmlab.com/mmcv/dist/cu111/torch1.10.0/index.html
+    pip install mmdet==2.19.1
+    cd ../../mmocr/
+    pip install -qr requirements.txt
+    pip install -v -e .
+```
+
+# 
---
+++ run.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+echo "Image Input:" $1;
+echo "Detection Output: " $2;
+echo "Recognition Output:" $3;
+o=$2/exp
+
+# rm -rf $3;
+# ls weights/yolov5/
+echo "Detecting..."
+m_best="./weights/yolov5/yolov5m6_fold3_best.pt"
+l_best="./weights/yolov5/yolov5l6_fold3_best.pt"
+# python3 source/det/yolov5/detect.py --weight $m_best $l_best --img 2304 --iou 0.15 --half --augment --conf-thres 0.4  --source $1 --save-txt --nosave --project $2 --exist-ok
+# python3 source/det/yolov5/detect.py --weight $m_best --img 360 --iou 0.15 --half --augment --conf-thres 0.4  --source $1 --save-txt --nosave --project $2 --exist-ok
+
+# echo "Cropping images..."
+# python3 source/crop_images.py --label_path $o --data_path $1
+
+rec_fold0='./weights/mmocr/fold0.pth'
+# rec_fold1='./weights/mmocr/fold1.pth'
+# rec_fold2='./weights/mmocr/fold2.pth'
+# rec_fold3='./weights/mmocr/fold3.pth'
+# rec_fold4='./weights/mmocr/fold4.pth'
+
+cfgs='source/config/mmocr/starn.py'
+
+echo "Recognizing..."
+# cd /usr/src/app/AIC-2021/4rangers-AIC/mmocr
+# python3 source/mmocr/tools/test.py $cfgs $rec_fold0 $o --out $o/res_fold0.pkl
+# python3 source/mmocr/tools/test.py $cfgs $rec_fold1 --out $o/res_fold1.pkl
+# python3 source/mmocr/tools/test.py $cfgs $rec_fold2 --out $o/res_fold2.pkl
+# python3 source/mmocr/tools/test.py $cfgs $rec_fold3 --out $o/res_fold3.pkl
+# python3 source/mmocr/tools/test.py $cfgs $rec_fold4 --out $o/res_fold4.pkl
+
+# python3 source/write_result.py --path $o 
+
+cp $o/e2e_result/* $3
+echo "Post processing..."
+python3 source/post_process/magic.py --path $3 --out $3
